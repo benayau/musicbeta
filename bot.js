6a7f8167771d59ea6027f0bb41d50c75146a203c
benayau <30296215+benayau@users.noreply.github.com>
2019-01-22 15:34:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -313,7 +313,7 @@ function play(guild, song) {
 
 
 client.on('message', message => {
-    if (message.content === 'help') {
+    if (message.content === 'shelp') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
         .setDescription('**برفكس البوت (!)**')
